AuScalableDroneCloud/pipeline-fracture

commit 9285653f4ada83dfc72e1297a8b37f0ff0f6d570

bbox_metadata

Jupyter/src/tools.py
@@ -17,7 +17,7 @@
 
 import os
 import sys
-import asdc
+#import asdc
 import pathlib
 import numpy as np
 from osgeo import gdal
@@ -96,7 +96,7 @@ class Tools:
     PASSW = '^JwtGk^xM79D#_6&QXPCtkzG6M5kwC'
     SHP = ''
     GEOTIF = ''
-    EXTEND = (0, 0)
+    EXTEND = None
     PROJ = None
     GEOT = None
     J_NAME = 'sample'
@@ -267,7 +267,7 @@ class Tools:
          show2 = w.VBox([tile, out2]) 
          display(show2)     
       
-    def TileImage(self, default=None):
+    def TileImage(self):
         if (len(self.DATA2) == 1):
             Tools.CheckTemp()
             btn = w.Button(description='Tile image')
@@ -287,11 +287,6 @@ class Tools:
     
             btn.on_click(Tile)
             bt2.on_click(NoTile)
-
-            if default == 0:
-                NoTile(self)
-            elif default == 1:
-                Tile(self)
             
             buttons = w.HBox([btn, bt2])
             show = w.VBox([buttons, out])
@@ -585,25 +580,25 @@ class Tools:
             print("written image ", filename)
             Tools.GEOTIF = path
             outdata = None
-
+       
 #WEBODM_part-------------------------------------------------------------------
     def GetAssets(self, project, task):
         Tools.FILE = []
         assests = ['orthophoto.tif', 'dsm.tif']
-        if not project: project = '622'
-        if not task: task = 'b4b2382f-1946-4593-99c7-a01615d9ebd4'
+        project = '622'
+        task = 'b4b2382f-1946-4593-99c7-a01615d9ebd4'
     
-        pathlib.Path(task).mkdir(parents=True, exist_ok=True)
         os.chdir(task)
+        pathlib.Path(task).mkdir(parents=True, exist_ok=True)
         for i in assests:
-            r = asdc.download_asset(i, project=project, task=task)
-            if r and "orthophoto" in i or "dsm" in i:
+            asdc.download_asset(project, task, i)
+            if "orthophoto" in i or "dsm" in i:
                 Tools.ASSETS.append( str(i) )     
                 print('added', i, 'to downloaded assets.')  
         if (len(Tools.ASSETS) > 0):
             Tools.FILE.append( Tools.ASSETS[0])
         else:
-            print('ERROR: Could not retrieve assets')
+            print('ERROR: Could not retrive assets')
             sys.exit()
                 
     def SelectAsset(self):     
@@ -620,7 +615,7 @@ class Tools:
                         Tools.FILE = []
                         Tools.FILE.append( d )
         file.observe(on_change)
-
+        
 #CSIRO DAP---------------------------------------------------------------------
     def MetaData(self):
         types = ["JSON", "CSIRO DAP (test)", "TERN"]
@@ -669,11 +664,11 @@ class Tools:
                 
         all_widgets[0].observe(change_user)
         all_widgets[1].observe(change_passw)
-
-
-
         
 
         
-
+        
+        
+        
+        
         

Jupyter/src/grass.py
@@ -1,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 17 10:45:39 2022
+
+@author: kel321
+"""
+
+import grass.script as grass
+
+
+
+
+def GrassNoData():
+    
+    grass.run_command('grass:r.mapcalculator',
+                      {"amap": gPb_rlayer,
+                       "formula": "if(A>0, 1, null())",
+                       "GRASS_REGION_PARAMETER": "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax),
+                       "GRASS_REGION_CELLSIZE_PARAMETER": 1,
+                       "outfile": mapcalc})
+    pass
+
+def GrassThinning():
+    pass
+
+def GrassR2Vec():
+    pass
+
+
+
+
+
+
+
+processing.runalg('grass7:r.thin',
+                  {"input": mapcalc,
+                   "GRASS_REGION_PARAMETER": "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax),
+                   "output": thinned})
+
+processing.runalg('grass7:r.to.vect',
+                  {"input": thinned,
+                   "type": 0,
+                   "GRASS_OUTPUT_TYPE_PARAMETER": 2,
+                   "GRASS_REGION_PARAMETER": "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax),
+                   "output": centerlines})

Jupyter/src/metadata.py
@@ -35,6 +35,10 @@ def CreateJSON(Tools):
         	"notebook": {
         		"file": "CoSh_ensemble_webodm.ipynb",
         		"version": 1.0,
+                "Bounding box":{
+                    "LL": 4,
+                    "UR": 5,
+                },
         		"parameters": {
                     "Projection ": Tools.PROJ,
                     "Geotransform ": Tools.GEOT,

Jupyter/src/processing.py
@@ -20,6 +20,7 @@ import cv2
 import time
 import numpy as np
 import itertools
+import skimage as sk
 
 from osgeo import gdal,osr, ogr, gdal_array
 from sknw import build_sknw
@@ -76,6 +77,22 @@ def CheckDetectionParams(offset):
             print("invalid entry in offset: ", off)
             new_offset.remove(off)
     return(new_offset)
+
+
+
+def Project2WGS84(pointX, pointY, pointZ, inputEPSG):
+    point = ogr.Geometry(ogr.wkbPoint)
+    point.AddPoint(pointX, pointY, pointZ)
+    outSpatialRef = osr.SpatialReference()
+    outSpatialRef.ImportFromEPSG(4326) #this is wgs84    
+    coordTransform = osr.CoordinateTransformation(inputEPSG, outSpatialRef)
+    point.Transform(coordTransform)
+    X = point.GetX()
+    Y = point.GetY()
+    Z = point.GetZ()
+    return(X, Y, Z)
+
+
 #==============================================================================
 def PrepareImages(Tools):
     Tools.RAW_IMG = []
@@ -88,17 +105,21 @@ def PrepareImages(Tools):
         dataset = None
         res = isinstance(img, str)
         if (res):
-            #Get the extend of the raster
             dataset = gdal.Open(img, gdal.GA_ReadOnly)
-            ulx, xres, xskew, uly, yskew, yres  = dataset.GetGeoTransform()
-            lrx = ulx + (dataset.RasterXSize * xres)
-            lry = uly + (dataset.RasterYSize * yres)
-            Tools.EXTEND = (lrx, lry)
+
         else:
             print('cannot resolve filename', img)
         if dataset:
             if dataset.GetProjection():
                 if dataset.GetGeoTransform():
+                    
+                    #Get the extend of the raster
+                    proj = osr.SpatialReference(wkt=dataset.GetProjection())
+                    ulx, xres, xskew, uly, yskew, yres  = dataset.GetGeoTransform()
+                    newX, newY, _ = Project2WGS84(uly, ulx, 0.0, proj)
+                    lrx = newX + (dataset.RasterXSize * xres)
+                    lry = newY + (dataset.RasterYSize * yres)   
+                    Tools.EXTEND = ((newX, newY), (lrx, lry) )
                     Tools.PROJ = dataset.GetProjection()
                     Tools.GEOT = dataset.GetGeoTransform()
                     noData = dataset.GetRasterBand(1).GetNoDataValue()
@@ -107,7 +128,13 @@ def PrepareImages(Tools):
                     bands = [dataset.GetRasterBand(k + 1) for k in range(dataset.RasterCount)]
                     DataType = dataset.GetRasterBand(1).DataType
                     driver = gdal.GetDriverByName('MEM')
-                    dst_ds = driver.CreateCopy('', dataset, strict=0)
+                    
+                    dst_ds = driver.CreateCopy('', dataset, strict=0)  
+                    for i, b in enumerate(bands):
+                        d = b.ReadAsArray()
+                        dst_ds.GetRasterBand(i+1).WriteArray(d.astype("uint8"))
+                    bands = [dst_ds.GetRasterBand(k + 1) for k in range(dst_ds.RasterCount)]
+                    
                     for i, b in enumerate(bands):        
                         stats = b.GetStatistics(True, True)
                         print("band ",i+1, ": min: ", stats[0], "max: ", stats[1])
@@ -254,7 +281,7 @@ def ReadImage(Tools):
             if ( Tools.HISTEQ ):
                 arr = np.uint8(gray)
                 gray = cv2.equalizeHist(arr)
-           
+
             if ( Tools.GAUSBL ):
                 gray = cv2.GaussianBlur(gray,(5,5),0)
            
@@ -276,7 +303,7 @@ def ReadImage(Tools):
             if ( Tools.SOBEL ):
                 x = cv2.Sobel(gray,cv2.CV_64F,1,0,ksize=5)
                 y = cv2.Sobel(gray,cv2.CV_64F,0,1,ksize=5)
-                gray = np.abs( (0.5*x) + (0.5*y) )
+                gray =  (0.5*x) + (0.5*y) 
                 
             if (Tools.INVERT):
                 gray = cv2.bitwise_not(gray)
@@ -284,8 +311,6 @@ def ReadImage(Tools):
             out.GetRasterBand(1).WriteArray( gray )
             ImgList.append(out)
         Tools.DATA2 = ImgList
-
-
 
 '''
 check consient size of images and retun dimension of smallest image
@@ -447,7 +472,7 @@ def DetectFeatures(Tools):
     negative        = Tools.NEGATI
     
     if (Tools.RIDGES):
-        print('detecting features with ', len(Tools.R_SYS), " systems.")
+        print('detecting features with ', len(Tools.R_SYS[0]), " systems.")
     t = time.time()
 
     # offset = CheckDetectionParams(offset)
@@ -481,8 +506,8 @@ def DetectFeatures(Tools):
              fp = zip(func_params)
              if (len(all_detec_combs) < os.cpu_count()):
                  mw = len(all_detec_combs)   
-           
-                 
+                
+             
              with ProcessPoolExecutor(max_workers = mw) as executor:
                  for r, o in executor.map(Detect, fp): 
                      thinned_f = mask(r, thin_mask(r))
@@ -503,13 +528,14 @@ def DetectFeatures(Tools):
             if (len(all_detec_combs) < os.cpu_count()):
                 mw = len(all_detec_combs)   
                 
-             
+            mw = 5
             with ProcessPoolExecutor(max_workers = mw) as executor:
                 for r, o in executor.map(Detect, fp): 
                     thinned_f = mask(r, thin_mask(r))
+                    #thinned_f = CleanIntensityMap(thinned_f)
                     detected = np.add(detected, thinned_f)  
          norm = np.zeros(img[0].shape, np.double)
-         normalized = cv2.normalize(detected, norm, 1.0, 0.0, cv2.NORM_MINMAX, dtype=cv2.CV_64F)          
+         normalized = cv2.normalize(detected, norm, 1.0, 0.0, cv2.NORM_MINMAX, dtype=cv2.CV_32F)          
          data.GetRasterBand(1).WriteArray(normalized)
          Tools.FEATURES.append(data)
          data = None
@@ -520,7 +546,19 @@ def DetectFeatures(Tools):
 #Enhancing edge/ridge ensembles------------------------------------------------
 '''
 
+
 '''
+def CleanIntensityMap(img):
+    # define the kernel
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
+    # opening the image
+    opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=3)
+    img =  img - opening 
+    p1, p99 = np.percentile(img, (1, 99))
+    J = sk.exposure.rescale_intensity(img, in_range=(p1, p99))
+    cleaned = sk.morphology.remove_small_objects(J, min_size=10, connectivity=2)     
+    return(cleaned)
+
 def SigmoidNonlinearity(image):
     ridges_norm_sig = np.zeros(image.shape, np.double)
     w,h = image.shape
@@ -550,7 +588,6 @@ def EnhanceEnsemble(Tools):
 def Threshholding(image, thresh, ksize):   
     print("Imgae thresholding")
     print(" Threshhold: ", thresh)
-    print(" Kernel size: ", ksize)
     w,h = image.shape
     #e_kernel = np.ones((5,5),np.uint8)#kernel for erosion
     
@@ -719,3 +756,96 @@ def WritePoints2SHP(graph, dataset, outputFile, tolerance):
             lyr.CreateFeature(feat) 
             feat.Destroy()
     ds = None
+    
+def DynamicRangeCompression(img_list, c = 40):
+    DRC = []
+    
+    def _dr1(img1):
+        img1[img1 == 255] = 254
+        img1=np.log(img1+ 1)
+        return img1
+
+    def _dr(frame, c):
+        return (c * frame).astype(np.uint8)
+
+    def chipka(bdr, gdr, rdr, img):
+        q = []
+        m, n, _ = img.shape
+        for i, j, k in zip(bdr, gdr, rdr):
+            q.append(list(zip(i, j, k)))
+        return np.array(q).astype(np.uint8)
+     
+    for img in img_list:
+        b, g, r = img[:, :, 0], img[:, :, 1], img[:, :, 2]
+        bdr1, gdr1, rdr1 = map(lambda x: _dr1(x), (b, g, r))
+ 
+        bdr, gdr, rdr = map(lambda x: _dr(x, c), (bdr1, gdr1, rdr1))
+        res = chipka(bdr, gdr, rdr, img)
+        DRC.append(res)
+    return(DRC)
+
+#------------------------------------------------------------------------------
+'''
+processing.runalg('grass:r.mapcalculator',
+                  {"amap": gPb_rlayer,
+                   "formula": "if(A>0, 1, null())",
+                   "GRASS_REGION_PARAMETER": "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax),
+                   "GRASS_REGION_CELLSIZE_PARAMETER": 1,
+                   "outfile": mapcalc})
+
+
+
+processing.runalg('grass7:r.thin',
+                  {"input": mapcalc,
+                   "GRASS_REGION_PARAMETER": "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax),
+                   "output": thinned})
+
+processing.runalg('grass7:r.to.vect',
+                  {"input": thinned,
+                   "type": 0,
+                   "GRASS_OUTPUT_TYPE_PARAMETER": 2,
+                   "GRASS_REGION_PARAMETER": "%f,%f,%f,%f" % (xmin, xmax, ymin, ymax),
+                   "output": centerlines})
+
+'''
+
+
+def HillShade(img):  
+    from matplotlib.colors import LightSource
+    import matplotlib.pyplot as plt
+    z = img
+    dx, dy = dem['dx'], dem['dy']
+    dy = 111200 * dy
+    dx = 111200 * dx * np.cos(np.radians(dem['ymin']))
+    
+    # Shade from the northwest, with the sun 45 degrees from horizontal
+    ls = LightSource(azdeg=315, altdeg=45)
+    cmap = plt.cm.gist_earth
+    
+    fig, axs = plt.subplots(nrows=4, ncols=3, figsize=(8, 9))
+    plt.setp(axs.flat, xticks=[], yticks=[])
+    
+    # Vary vertical exaggeration and blend mode and plot all combinations
+    for col, ve in zip(axs.T, [0.1, 1, 10]):
+        # Show the hillshade intensity image in the first row
+        col[0].imshow(ls.hillshade(z, vert_exag=ve, dx=dx, dy=dy), cmap='gray')
+    
+        # Place hillshaded plots with different blend modes in the rest of the rows
+        for ax, mode in zip(col[1:], ['hsv', 'overlay', 'soft']):
+            rgb = ls.shade(z, cmap=cmap, blend_mode=mode,
+                           vert_exag=ve, dx=dx, dy=dy)
+            ax.imshow(rgb)
+    
+    
+    def PolylineFitting(img_list, filename, tolerance):
+        import numpy.polynomial.polynomial as poly
+        for img in img_list:
+            data = img.GetRasterBand(1).ReadAsArray()
+            
+            nb_components, output, stats, centroids  = cv2.connectedComponentsWithStats(data.astype("uint8"), 4)
+    
+            for i in nb_components:
+                
+                print(i)
+                #coefs = poly.polyfit(x, y, 4)
+                #ffit = poly.polyval(x_new, coefs)

Jupyter/src/traceSkeleton.py
@@ -1,0 +1,333 @@
+# trace_skeleton.py
+# Trace skeletonization result into polylines
+#
+# Lingdong Huang 2020
+
+import numpy as np
+
+# binary image thinning (skeletonization) in-place.
+# implements Zhang-Suen algorithm.
+# http://agcggs680.pbworks.com/f/Zhan-Suen_algorithm.pdf
+# @param im   the binary image
+def thinningZS(im):
+  prev = np.zeros(im.shape,np.uint8);
+  while True:
+    im = thinningZSIteration(im,0);
+    im = thinningZSIteration(im,1)
+    diff = np.sum(np.abs(prev-im));
+    if not diff:
+      break
+    prev = im
+  return im
+
+# 1 pass of Zhang-Suen thinning 
+def thinningZSIteration(im, iter):
+  marker = np.zeros(im.shape,np.uint8);
+  for i in range(1,im.shape[0]-1):
+    for j in range(1,im.shape[1]-1):
+      p2 = im[(i-1),j]  ;
+      p3 = im[(i-1),j+1];
+      p4 = im[(i),j+1]  ;
+      p5 = im[(i+1),j+1];
+      p6 = im[(i+1),j]  ;
+      p7 = im[(i+1),j-1];
+      p8 = im[(i),j-1]  ;
+      p9 = im[(i-1),j-1];
+      A  = (p2 == 0 and p3) + (p3 == 0 and p4) + \
+           (p4 == 0 and p5) + (p5 == 0 and p6) + \
+           (p6 == 0 and p7) + (p7 == 0 and p8) + \
+           (p8 == 0 and p9) + (p9 == 0 and p2);
+      B  = p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9;
+      m1 = (p2 * p4 * p6) if (iter == 0 ) else (p2 * p4 * p8);
+      m2 = (p4 * p6 * p8) if (iter == 0 ) else (p2 * p6 * p8);
+
+      if (A == 1 and (B >= 2 and B <= 6) and m1 == 0 and m2 == 0):
+        marker[i,j] = 1;
+
+  return np.bitwise_and(im,np.bitwise_not(marker))
+
+
+def thinningSkimage(im):
+  from skimage.morphology import skeletonize
+  return skeletonize(im).astype(np.uint8)
+
+def thinning(im):
+  try:
+    return thinningSkimage(im)
+  except:
+    return thinningZS(im)
+
+#check if a region has any white pixel
+def notEmpty(im, x, y, w, h):
+  return np.sum(im) > 0
+
+
+# merge ith fragment of second chunk to first chunk
+# @param c0   fragments from first  chunk
+# @param c1   fragments from second chunk
+# @param i    index of the fragment in first chunk
+# @param sx   (x or y) coordinate of the seam
+# @param isv  is vertical, not horizontal?
+# @param mode 2-bit flag, 
+#             MSB = is matching the left (not right) end of the fragment from first  chunk
+#             LSB = is matching the right (not left) end of the fragment from second chunk
+# @return     matching successful?             
+# 
+def mergeImpl(c0, c1, i, sx, isv, mode):
+
+  B0 = (mode >> 1 & 1)>0; # match c0 left
+  B1 = (mode >> 0 & 1)>0; # match c1 left
+  mj = -1;
+  md = 4; # maximum offset to be regarded as continuous
+  
+  p1 = c1[i][0 if B1 else -1];
+  
+  if (abs(p1[isv]-sx)>0): # not on the seam, skip
+    return False
+  
+  # find the best match
+  for j in range(len(c0)):
+    p0 = c0[j][0 if B0 else -1];
+    if (abs(p0[isv]-sx)>1): # not on the seam, skip
+      continue
+    
+    d = abs(p0[not isv] - p1[not isv]);
+    if (d < md):
+      mj = j;
+      md = d;
+
+  if (mj != -1): # best match is good enough, merge them
+    if (B0 and B1):
+      c0[mj] = list(reversed(c1[i])) + c0[mj]
+    elif (not B0 and B1):
+      c0[mj]+=c1[i]
+    elif (B0 and not B1):
+      c0[mj] = c1[i] + c0[mj]
+    else:
+      c0[mj] += list(reversed(c1[i]))
+    
+    c1.pop(i);
+    return True;
+  return False;
+
+HORIZONTAL = 1;
+VERTICAL = 2;
+
+# merge fragments from two chunks
+# @param c0   fragments from first  chunk
+# @param c1   fragments from second chunk
+# @param sx   (x or y) coordinate of the seam
+# @param dr   merge direction, HORIZONTAL or VERTICAL?
+# 
+def mergeFrags(c0, c1, sx, dr):
+  for i in range(len(c1)-1,-1,-1):
+    if (dr == HORIZONTAL):
+      if (mergeImpl(c0,c1,i,sx,False,1)):continue;
+      if (mergeImpl(c0,c1,i,sx,False,3)):continue;
+      if (mergeImpl(c0,c1,i,sx,False,0)):continue;
+      if (mergeImpl(c0,c1,i,sx,False,2)):continue;
+    else:
+      if (mergeImpl(c0,c1,i,sx,True,1)):continue;
+      if (mergeImpl(c0,c1,i,sx,True,3)):continue;
+      if (mergeImpl(c0,c1,i,sx,True,0)):continue;
+      if (mergeImpl(c0,c1,i,sx,True,2)):continue;      
+    
+  c0 += c1
+
+
+# recursive bottom: turn chunk into polyline fragments;
+# look around on 4 edges of the chunk, and identify the "outgoing" pixels;
+# add segments connecting these pixels to center of chunk;
+# apply heuristics to adjust center of chunk
+# 
+# @param im   the bitmap image
+# @param x    left of   chunk
+# @param y    top of    chunk
+# @param w    width of  chunk
+# @param h    height of chunk
+# @return     the polyline fragments
+# 
+def chunkToFrags(im, x, y, w, h):
+  frags = []
+  on = False; # to deal with strokes thicker than 1px
+  li=-1; lj=-1;
+  
+  # walk around the edge clockwise
+  for k in range(h+h+w+w-4):
+    i=0; j=0;
+    if (k < w):
+      i = y+0; j = x+k;
+    elif (k < w+h-1):
+      i = y+k-w+1; j = x+w-1;
+    elif (k < w+h+w-2):
+      i = y+h-1; j = x+w-(k-w-h+3); 
+    else:
+      i = y+h-(k-w-h-w+4); j = x+0;
+    
+    if (im[i,j]): # found an outgoing pixel
+      if (not on):     # left side of stroke
+        on = True;
+        frags.append([[j,i],[x+w//2,y+h//2]])
+    else:
+      if (on):# right side of stroke, average to get center of stroke
+        frags[-1][0][0]= (frags[-1][0][0]+lj)//2;
+        frags[-1][0][1]= (frags[-1][0][1]+li)//2;
+        on = False;
+    li = i;
+    lj = j;
+  
+  if (len(frags) == 2): # probably just a line, connect them
+    f = [frags[0][0],frags[1][0]];
+    frags.pop(0);
+    frags.pop(0);
+    frags.append(f);
+  elif (len(frags) > 2): # it's a crossroad, guess the intersection
+    ms = 0;
+    mi = -1;
+    mj = -1;
+    # use convolution to find brightest blob
+    for i in range(y+1,y+h-1):
+      for j in range(x+1,x+w-1):
+        s = \
+          (im[i-1,j-1]) + (im[i-1,j]) +(im[i-1,j+1])+\
+          (im[i,j-1]  ) +   (im[i,j]) +    (im[i,j+1])+\
+          (im[i+1,j-1]) + (im[i+1,j]) +  (im[i+1,j+1]);
+        if (s > ms):
+          mi = i;
+          mj = j;
+          ms = s;
+        elif (s == ms and abs(j-(x+w//2))+abs(i-(y+h//2)) < abs(mj-(x+w//2))+abs(mi-(y+h//2))):
+          mi = i;
+          mj = j;
+          ms = s;
+
+    if (mi != -1):
+      for i in range(len(frags)):
+        frags[i][1]=[mj,mi]
+  return frags;
+
+
+# Trace skeleton from thinning result.
+# Algorithm:
+# 1. if chunk size is small enough, reach recursive bottom and turn it into segments
+# 2. attempt to split the chunk into 2 smaller chunks, either horizontall or vertically;
+#    find the best "seam" to carve along, and avoid possible degenerate cases
+# 3. recurse on each chunk, and merge their segments
+# 
+# @param im      the bitmap image
+# @param x       left of   chunk
+# @param y       top of    chunk
+# @param w       width of  chunk
+# @param h       height of chunk
+# @param csize   chunk size
+# @param maxIter maximum number of iterations
+# @param rects   if not null, will be populated with chunk bounding boxes (e.g. for visualization)
+# @return        an array of polylines
+# 
+def traceSkeleton(im, x, y, w, h, csize, maxIter, rects):
+  
+  frags = []
+  
+  if (maxIter == 0): # gameover
+    return frags;
+  if (w <= csize and h <= csize): # recursive bottom
+    frags += chunkToFrags(im,x,y,w,h);
+    return frags;
+  
+  ms = im.shape[0]+im.shape[1]; # number of white pixels on the seam, less the better
+  mi = -1; # horizontal seam candidate
+  mj = -1; # vertical   seam candidate
+  
+  if (h > csize): # try splitting top and bottom
+    for i in range(y+3,y+h-3):
+      if (im[i,x]  or im[(i-1),x]  or im[i,x+w-1]  or im[(i-1),x+w-1]):
+        continue
+      
+      s = 0;
+      for j in range(x,x+w):
+        s += im[i,j];
+        s += im[(i-1),j];
+      
+      if (s < ms):
+        ms = s; mi = i;
+      elif (s == ms  and  abs(i-(y+h//2))<abs(mi-(y+h//2))):
+        # if there is a draw (very common), we want the seam to be near the middle
+        # to balance the divide and conquer tree
+        ms = s; mi = i;
+  
+  if (w > csize): # same as above, try splitting left and right
+    for j in range(x+3,x+w-2):
+      if (im[y,j] or im[(y+h-1),j] or im[y,j-1] or im[(y+h-1),j-1]):
+        continue
+      
+      s = 0;
+      for i in range(y,y+h):
+        s += im[i,j];
+        s += im[i,j-1];
+      if (s < ms):
+        ms = s;
+        mi = -1; # horizontal seam is defeated
+        mj = j;
+      elif (s == ms  and  abs(j-(x+w//2))<abs(mj-(x+w//2))):
+        ms = s;
+        mi = -1;
+        mj = j;
+
+  nf = []; # new fragments
+  if (h > csize  and  mi != -1): # split top and bottom
+    L = [x,y,w,mi-y];    # new chunk bounding boxes
+    R = [x,mi,w,y+h-mi];
+    
+    if (notEmpty(im,L[0],L[1],L[2],L[3])): # if there are no white pixels, don't waste time
+      if(rects!=None):rects.append(L);
+      nf += traceSkeleton(im,L[0],L[1],L[2],L[3],csize,maxIter-1,rects) # recurse
+    
+    if (notEmpty(im,R[0],R[1],R[2],R[3])):
+      if(rects!=None):rects.append(R);
+      mergeFrags(nf,traceSkeleton(im,R[0],R[1],R[2],R[3],csize,maxIter-1,rects),mi,VERTICAL);
+    
+  elif (w > csize  and  mj != -1): # split left and right
+    L = [x,y,mj-x,h];
+    R = [mj,y,x+w-mj,h];
+    if (notEmpty(im,L[0],L[1],L[2],L[3])):
+      if(rects!=None):rects.append(L);
+      nf+=traceSkeleton(im,L[0],L[1],L[2],L[3],csize,maxIter-1,rects);
+    
+    if (notEmpty(im,R[0],R[1],R[2],R[3])):
+      if(rects!=None):rects.append(R);
+      mergeFrags(nf,traceSkeleton(im,R[0],R[1],R[2],R[3],csize,maxIter-1,rects),mj,HORIZONTAL);
+    
+  frags+=nf;
+  if (mi == -1  and  mj == -1): # splitting failed! do the recursive bottom instead
+    frags += chunkToFrags(im,x,y,w,h);
+  
+  return frags
+
+
+if __name__ == "__main__":
+  import cv2
+  import random
+
+  im0 = cv2.imread("../test_images/opencv-thinning-src-img.png")
+
+  im = (im0[:,:,0]>128).astype(np.uint8)
+
+  # for i in range(im.shape[0]):
+  #   for j in range(im.shape[1]):
+  #     print(im[i,j],end="")
+  #   print("")
+  # print(np.sum(im),im.shape[0]*im.shape[1])
+  im = thinning(im);
+
+  # cv2.imshow('',im*255);cv2.waitKey(0)
+
+  rects = []
+  polys = traceSkeleton(im,0,0,im.shape[1],im.shape[0],10,999,rects)
+  
+
+  for l in polys:
+    c = (200*random.random(),200*random.random(),200*random.random())
+    for i in range(0,len(l)-1):
+      cv2.line(im0,(l[i][0],l[i][1]),(l[i+1][0],l[i+1][1]),c)
+
+  cv2.imshow('',im0);cv2.waitKey(0)

commit 6a5046c9aabfcc6f837acc09a77c2f1db554c449

Restore all my fixes to tools.py regressed in 9285653f

Jupyter/src/tools.py
@@ -17,7 +17,6 @@
 
 import os
 import sys
-#import asdc
 import pathlib
 import numpy as np
 from osgeo import gdal
@@ -267,7 +266,7 @@ class Tools:
          show2 = w.VBox([tile, out2]) 
          display(show2)     
       
-    def TileImage(self):
+    def TileImage(self, default=None):
         if (len(self.DATA2) == 1):
             Tools.CheckTemp()
             btn = w.Button(description='Tile image')
@@ -287,6 +286,11 @@ class Tools:
     
             btn.on_click(Tile)
             bt2.on_click(NoTile)
+
+            if default == 0:
+                NoTile(self)
+            elif default == 1:
+                Tile(self)
             
             buttons = w.HBox([btn, bt2])
             show = w.VBox([buttons, out])
@@ -585,20 +589,21 @@ class Tools:
     def GetAssets(self, project, task):
         Tools.FILE = []
         assests = ['orthophoto.tif', 'dsm.tif']
-        project = '622'
-        task = 'b4b2382f-1946-4593-99c7-a01615d9ebd4'
+        if not project: project = '622'
+        if not task: task = 'b4b2382f-1946-4593-99c7-a01615d9ebd4'
     
-        os.chdir(task)
         pathlib.Path(task).mkdir(parents=True, exist_ok=True)
+        os.chdir(task)
+        import asdc
         for i in assests:
-            asdc.download_asset(project, task, i)
-            if "orthophoto" in i or "dsm" in i:
+            r = asdc.download_asset(i, project=project, task=task)
+            if r and "orthophoto" in i or "dsm" in i:
                 Tools.ASSETS.append( str(i) )     
                 print('added', i, 'to downloaded assets.')  
         if (len(Tools.ASSETS) > 0):
             Tools.FILE.append( Tools.ASSETS[0])
         else:
-            print('ERROR: Could not retrive assets')
+            print('ERROR: Could not retrieve assets')
             sys.exit()
                 
     def SelectAsset(self):     